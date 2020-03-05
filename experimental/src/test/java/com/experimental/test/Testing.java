4f374c707deb30c7571a0377b845ff2473671cbb
May Love <alove2@wisc.edu>
2020-03-05 13:43:12 -0500

    test project

---
+++ experimental/src/test/java/com/experimental/test/Testing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.experimental.test;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author may love
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class Testing {
+    @Test
+    public void bakaTest(){
+        assertTrue("true",true);
+    }
+}
